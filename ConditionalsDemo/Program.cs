a6ff86edc75ab1cdaaeb865b05aaa62488a57703
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-10-11 14:47:31 -0600

    First example completed.

--- ConditionalsDemo/Program.cs
+++ ConditionalsDemo/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            var hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
         }
     }
 }
